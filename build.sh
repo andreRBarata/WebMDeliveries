a64cc807040169fefca3f2da6678aa178470f1c7
André Barata <andre.quina.barata@gmail.com>
2017-04-24 19:24:12 +0100

    Folder move

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cd deliveries
 cordova build browser
-sudo docker build -t andrerbarata/webmdeliveries .
+sudo docker build -t andrerbarata/webmdeliveries ../
 sudo docker stop admiring_minsky
 sudo docker rm admiring_minsky
 sudo docker run --net="host" -e DEV=true --name=admiring_minsky -p 80:80 andrerbarata/webmdeliveries
